947549220bf0f5ca77cdcf31a861536438fc266b
thakurrachitsingh <59912926+thakurrachitsingh@users.noreply.github.com>
2020-11-30 22:08:43 +0530

    TextCard updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -37,6 +37,7 @@ class _HomePageState extends State<HomePage> {
                         TextCard(),
                         TextCard(),
                         TextCard(),
+                        TextCard(),
                       ],
                     ),
                   ),
